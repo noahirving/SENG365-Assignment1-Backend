93d5ccfcfff45c89b70e7a48f83262a4ae737def
Noah Irving <nji29@uclive.ac.nz>
2021-03-30 22:23:45 +1300

    Testing express-openapi-validator.

--- config/express.js
+++ config/express.js
@@ -22,7 +22,7 @@ module.exports = function () {
     app.use(
         OpenApiValidator.middleware({
             apiSpec: spec,
-            validateSecurity: false,
+            //validateSecurity: false,
             $refParser: {
                 mode: 'dereference'
             },
