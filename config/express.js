3b0cf9d88f6e5f1294a46679701ad91deec9617a
Noah Irving <nji29@uclive.ac.nz>
2021-03-30 21:46:53 +1300

    Testing express-openapi-validator.

--- config/express.js
+++ config/express.js
@@ -17,7 +17,7 @@ module.exports = function () {
 
 
     // Validates requests using API spec
-    const spec = path.join(__dirname, '../app/resources/seng365_event_site_api_spec.yaml');
+    const spec = path.join(__dirname, '../app/resources/seng365_event_site_api_spec.json');
     app.use('/spec', express.static(spec));
     app.use(
         OpenApiValidator.middleware({
